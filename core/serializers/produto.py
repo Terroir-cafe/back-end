2f06ab33dd67706199c5e2baafd2e921e1d6fc5a
Gianlucagadotti <gianlucagadotti@gmail.com>
2026-05-18 14:52:57 -0300

    Feat 2:  #3 Adiciona permissões e configurações de admin para produtos e categorias

--- core/serializers/produto.py
+++ core/serializers/produto.py
@@ -6,7 +6,6 @@ from uploader.serializers import ImageSerializer
 
 
 class ProdutoSerializer(ModelSerializer):
-
     capa_attachment_key = SlugRelatedField(
         source='capa',
         queryset=Image.objects.all(),
@@ -25,11 +24,17 @@ class ProdutoListSerializer(ModelSerializer):
     class Meta:
         model = Produto
         fields = '__all__'
-        fields = ['id', 'nome', 'preco', 'categoria',]
+        fields = [
+            'id',
+            'nome',
+            'preco',
+            'categoria',
+        ]
 
 
 class ProdutoRetrieveSerializer(ModelSerializer):
     capa = ImageSerializer(required=False)
+
     class Meta:
         model = Produto
         fields = '__all__'
